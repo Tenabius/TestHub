13d14c91e1003abbbc3d793e3bef55bcf2cf8a9a
Yauhen Daneka <den130628@gmail.com>
2023-03-18 09:06:49 +0300

    Refactored flow of creating matching question

--- src/TestHub.ApplicationCore/Entities/MatchingQuestion.cs
+++ src/TestHub.ApplicationCore/Entities/MatchingQuestion.cs
@@ -6,32 +6,30 @@ namespace TestHub.Core.Entities
     {
         private readonly List<Stem> _stems;
         public IReadOnlyList<Stem> Stems => _stems.AsReadOnly();
-        private readonly List<Response> _responses;
-        public IReadOnlyList<Response> Responses => _responses.AsReadOnly();
 
 #pragma warning disable CS8618
         private MatchingQuestion() { }
 #pragma warning restore 
 
-        private MatchingQuestion(string directions, IList<Stem> stems, IList<Response> responses)
+        private MatchingQuestion(string directions, IList<Stem> stems)
             : base(directions)
         {
             _stems = stems.ToList();
-            _responses = responses.ToList();
         }
 
-        public static MatchingQuestion Create(string directions, IList<Stem> stems, IList<Response> responses)
+        public static MatchingQuestion Create(string directions, IList<Stem> stems)
         {
             Requires.NotNullOrEmpty(directions, nameof(directions));
+            Requires.Range(stems.Count >= 2, nameof(stems), "Matching quest must have two or greater number of stems");
 
-            return new(directions, stems, responses);
+            return new(directions, stems);
         }
 
         public override bool EvaluateAnswer(Answer submittedAnswer)
         {
             if (submittedAnswer is MatchingAnswer answer)
             {
-                return answer.SubmittedResponses.All(a => a.Stem.CorrectResponse == a.Response);
+                return answer.SubmittedResponses.All(a => a.Stem.Response == a.Response);
             }
 
             throw new InvalidCastException(nameof(submittedAnswer));
@@ -40,24 +38,24 @@ namespace TestHub.Core.Entities
         public sealed class Stem : BaseEntity
         {
             public string Content { get; private set; }
-            public Response CorrectResponse { get; private set; }
+            public Response Response { get; private set; }
 
 #pragma warning disable CS8618
             private Stem() { }
 #pragma warning restore 
 
-            private Stem(string content, Response correctResponse)
+            private Stem(string content, Response response)
             {
                 Content = content;
-                CorrectResponse = correctResponse;
+                Response = response;
             }
 
-            public static Stem Create(string content, Response correctResponse)
+            public static Stem Create(string content, Response response)
             {
                 Requires.NotNullOrEmpty(content, nameof(content));
-                Requires.NotNull(correctResponse, nameof(correctResponse));
+                Requires.NotNull(response, nameof(response));
 
-                return new(content, correctResponse);
+                return new(content, response);
             }
         }
 
