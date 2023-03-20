81e109084e002d20bcc0700568934d9b45b2982e
Yauhen Daneka <den130628@gmail.com>
2023-03-20 17:33:05 +0300

    Working on controller

--- src/TestHub.Web/Areas/TestTaker/Controllers/TestTakerController.cs
+++ src/TestHub.Web/Areas/TestTaker/Controllers/TestTakerController.cs
@@ -1,26 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using TestHub.Core.Entities;
 using TestHub.Core.Interfaces;
+using TestHub.Web.Areas.TestTaker.Models;
 
 namespace TestHub.Web.Controllers
 {
-    public class TestController : Controller
+    public class TestTakerController : Controller
     {
         private readonly IRepository<Test> _repository;
 
-        public TestController(IRepository<Test> repository)
+        public TestTakerController(IRepository<Test> repository)
         {
             _repository = repository;
         }
 
-        public IActionResult Index(int Id = 1)
+        public IActionResult Index(int id)
         {
             return View();
             //return View(_repository.GetById(Id)?.GetQuestionsContents());
         }
 
         [HttpPost]
-        public IActionResult Submit([FromForm] List<Answer> questionForms)
+        public IActionResult Submit([FromForm] List<AnswerViewModel> answers)
         {
             var questions = _repository.GetById(1)?.Questions;
             int result = 0;
