120da9e4e01af0f6bfbe29fc94f36f9d30828be8
Yauhen Daneka <den130628@gmail.com>
2023-03-22 17:16:13 +0300

    Implemented Index action

--- src/TestHub.Web/Areas/TestTaker/Controllers/TestTakerController.cs
+++ src/TestHub.Web/Areas/TestTaker/Controllers/TestTakerController.cs
@@ -1,30 +1,65 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using TestHub.Core.Entities;
 using TestHub.Core.Interfaces;
+using TestHub.Infrastructure;
 using TestHub.Web.Areas.TestTaker.Models;
 
 namespace TestHub.Web.Controllers
 {
     public class TestTakerController : Controller
     {
-        private readonly IRepository<Test> _repository;
+        private readonly IRepository<Test> _testsRepository;
+        private readonly IRepository<TestAttemptInfo> _attemptsRepository;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public TestTakerController(IRepository<Test> repository)
+        public TestTakerController(IRepository<Test> testsRepository,
+            IRepository<TestAttemptInfo> attemptsRepository,
+            UserManager<IdentityUser> userManager)
         {
-            _repository = repository;
+            _testsRepository = testsRepository;
+            _attemptsRepository = attemptsRepository;
+            _userManager = userManager;
         }
 
-        public IActionResult Index(int id)
+        public async Task<IActionResult> Index(int id, bool startTest = false)
         {
-            return View();
-            //return View(_repository.GetById(Id)?.GetQuestionsContents());
+            var test = await _testsRepository.GetByIdAsync(id);
+            if (test == null)
+            {
+                return NotFound();
+            }
+
+            if (!startTest)
+            {
+                return View("TestInfo");
+            }
+
+            if (HttpContext.User.Identity is { } identity
+                && identity.IsAuthenticated)
+            {
+                var user = await _userManager.GetUserAsync(HttpContext.User);
+
+                var testAttempt = new TestAttemptInfo(
+                    user!,
+                    test,
+                    DateTimeOffset.UtcNow,
+                    test.Duration + TimeSpan.FromMinutes(10));
+
+                await _attemptsRepository.CreateAsync(testAttempt);
+
+                ViewData["testAttemptId"] = testAttempt.Id;
+            }
+
+            return View(test);
         }
 
         [HttpPost]
-        public IActionResult Submit([FromForm] List<AnswerViewModel> answers)
+        public async Task<IActionResult> Submit([FromForm] List<AnswerViewModel> answers, [FromForm] int? testAttepmtId)
         {
-            var questions = _repository.GetById(1)?.Questions;
+            var testAttemptInfo = await _attemptsRepository.GetByIdAsync(1);
+            var questions = _testsRepository.GetById(1)?.Questions;
             int result = 0;
             //foreach (var questionForm in questionForms)
             //{
