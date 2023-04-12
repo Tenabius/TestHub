8b938f5e3924894faff6687187a2e714d632b097
Yauhen Daneka <den130628@gmail.com>
2023-04-12 20:23:00 +0300

    Moved _MasterLayout.cshtml to ...\Shared and configured RazorEngine to using that folder

---
+++ src/TestHub.Web/Configuration/RazorEngineConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using TestHub.Web.Interfaces;
+
+namespace TestHub.Web.Configuration
+{
+    public class RazorEngineConfiguration : IApplicationConfigurator
+    {
+        public void ApplyConfiguration(WebApplicationBuilder builder)
+        {
+            builder.Services.Configure<RazorViewEngineOptions>(o =>
+            {
+                o.ViewLocationFormats.Add("~/Shared/{0}" + RazorViewEngine.ViewExtension);
+            });
+        }
+    }
+}
