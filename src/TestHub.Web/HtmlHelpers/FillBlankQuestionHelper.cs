9c07d0456871d9ff9536cf98d57c034a57e542dc
Yauhen Daneka <den130628@gmail.com>
2023-04-17 07:55:52 +0300

    Working on FillBlankQuestion View

---
+++ src/TestHub.Web/HtmlHelpers/FillBlankQuestionHelper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TestHub.Web.HtmlHelpers
+{
+    public static class FillBlankQuestionHelper
+    {
+        public static HtmlString Format(this IHtmlHelper html, string context)
+        {
+            string result = "";
+            return new HtmlString(result);
+        }
+    }
+}
