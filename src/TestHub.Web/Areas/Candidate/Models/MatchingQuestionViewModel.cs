2ef74496def416eac7805308000d61d9dceacc67
Yauhen Daneka <den130628@gmail.com>
2023-04-21 15:11:31 +0300

    Working on partial view for matching question and changed its viewmodel

--- src/TestHub.Web/Areas/Candidate/Models/MatchingQuestionViewModel.cs
+++ src/TestHub.Web/Areas/Candidate/Models/MatchingQuestionViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using AutoMapper.Configuration.Annotations;
 using TestHub.Core.Entities;
 
 namespace TestHub.Web.Areas.Candidate.Models
@@ -6,21 +7,23 @@ namespace TestHub.Web.Areas.Candidate.Models
     [AutoMap(typeof(MatchingQuestion))]
     public class MatchingQuestionViewModel : QuestionViewModel
     {
-        public List<StemViewModel>? Stems { get; set; }
-        public List<ResponseViewModel>? Responses { get; set; }
+        public List<StemViewModel> Stems { get; set; } = new();
+        public List<ResponseViewModel> Responses { get; set; } = new();
 
         [AutoMap(typeof(MatchingQuestion.Stem))]
         public class StemViewModel
         {
             public int? Id { get; set; }
-            public string? Stem { get; set; }
+            public string? Content { get; set; }
+            public ResponseViewModel? CorrectResponse { get; set; }
+            public ResponseViewModel? SubmittedResponse { get; set; }
         }
 
         [AutoMap(typeof(MatchingQuestion.Response))]
         public class ResponseViewModel
         {
             public int? Id { get; set; }
-            public string? Response { get; set; }
+            public string? Content { get; set; }
         }
     }
 }
