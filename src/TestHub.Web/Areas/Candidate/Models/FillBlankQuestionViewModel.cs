2c1095996f44869b9fdb39064eabe9efad2d6a8c
Yauhen Daneka <den130628@gmail.com>
2023-04-21 19:06:42 +0300

    Forgotten push)

--- src/TestHub.Web/Areas/Candidate/Models/FillBlankQuestionViewModel.cs
+++ src/TestHub.Web/Areas/Candidate/Models/FillBlankQuestionViewModel.cs
@@ -12,6 +12,7 @@ namespace TestHub.Web.Areas.Candidate.Models
         [AutoMap(typeof(FillBlankQuestion.Blank))]
         public class BlankViewModel
         {
+            public int? Id { get; set; }
             public int? InnerId { get; set; }
             public string? SubmittedAnswer { get; set; }
             public string? CorrectAnswer { get; set; }
